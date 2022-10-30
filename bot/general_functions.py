ad51cdedeb88ea3c33a6387ae89ae9a594b26600
Alexander Zharyuk <alexander.zharyuk@gmail.com>
2022-10-30 02:00:22 +0200

    Add function for add to db new shifts on next week

--- bot/general_functions.py
+++ bot/general_functions.py
@@ -2,6 +2,7 @@ import os
 import sqlite3
 
 from datetime import datetime, timedelta
+from collections import defaultdict
 
 
 def get_weekly_shifts(week: str) -> list[set]:
@@ -15,7 +16,7 @@ def get_weekly_shifts(week: str) -> list[set]:
         date_of_week_beginning = today - timedelta(days=today.weekday())
         date_of_week_ending = date_of_week_beginning + timedelta(days=6)
     elif week == "next":
-        today = datetime(2022, 11, 2)
+        today = datetime.today()
         day_number = today.weekday()
         if not day_number:
             date_of_week_beginning = today
@@ -42,8 +43,8 @@ def get_daily_shifts() -> str:
     """
     Return daily shift info.
     """
-    # database_name = os.environ["DATABASE_NAME"]
-    connection = sqlite3.connect("shifts.sqlite3")
+    database_name = os.environ["DATABASE_NAME"]
+    connection = sqlite3.connect(database_name)
     cursor = connection.cursor()
 
     today = datetime.today()
@@ -54,3 +55,48 @@ def get_daily_shifts() -> str:
     """
     cursor.execute(query)
     return cursor.fetchall()
+
+
+def parse_shifts_text(text: str) -> dict:
+    """
+    Parse text from user to dict-format.
+    """
+    shifts = text.split("\n")
+    schedule = defaultdict(list)
+    shift_day = None
+
+    for shift in shifts:
+        if not shift:
+            continue
+
+        if len(shift.split("-")) > 2:
+            shift_day = datetime.strptime(
+                shift.replace(":", ""),
+                "%d-%m-%Y"
+            ).date()
+            continue
+
+        schedule[shift_day].append(shift)
+
+    return dict(schedule)
+
+
+def update_next_week_shifts(text: str):
+    """
+    Record to DB shifts on next week.
+    """
+    schedule = parse_shifts_text(text)
+    database_name = os.environ["DATABASE_NAME"]
+    connection = sqlite3.connect(database_name)
+    cursor = connection.cursor()
+
+    for shift_date, shift_timings in schedule.items():
+        for time in shift_timings:
+            query = f"""
+                INSERT INTO shifts(
+                shift_date, shift_time_starts, shift_time_ends)
+                 VALUES (?, ?, ?)
+                """
+            shift_start, shift_end = time.split(" - ")
+            cursor.execute(query, (shift_date, shift_start, shift_end))
+            connection.commit()
